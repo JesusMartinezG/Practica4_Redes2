3f6df1694f36b9ad28257a26641b2eb36f8574c9
JesusMartinezG <gsusmag@gmail.com>
2020-05-08 20:32:38 -0500

    http

--- http/http_client.py
+++ http/http_client.py
@@ -1,28 +1,76 @@
 import http.client
+import logging
+import json
+from time import sleep
 
-# get http server ip
-from pip._vendor.distlib.compat import raw_input
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(asctime)-10s) %(message)s',
+                    )
+
+def printResponse(resp):
+    logging.debug('{} {} {}'.format(resp._method, resp.status, resp.reason))
+    print(resp.read())
 
 http_server = '127.0.0.1:8000'
 # create a connection
 conn = http.client.HTTPConnection(http_server)
 
-while 1:
-    cmd = raw_input('input command (ex. GET index.html): ')
-    cmd = cmd.split()
+conn.request('GET', '/')
+resp = conn.getresponse()
+printResponse(resp)
+sleep(2.0)
 
-    if cmd[0] == 'exit':  # tipe exit to end it
-        break
+conn.request('GET', '/noexiste.html')
+resp = conn.getresponse()
+printResponse(resp)
+sleep(2.0)
 
-        # request command to server
-    conn.request(cmd[0], cmd[1])
+conn.request('HEAD', '/index.html')
+resp = conn.getresponse()
+printResponse(resp)
+print(resp.headers)
+sleep(2.0)
 
-    # get response from server
-    rsp = conn.getresponse()
+header = {'Content-type': 'application/json'}
+info = {'variable': 'valor'}
+json_data = json.dumps(info)
+conn.request('POST', '/', json_data, header)
+resp = conn.getresponse()
+printResponse(resp)
+sleep(1.0)
 
-    # print server response and data
-    print(rsp.status, rsp.reason)
-    data_received = rsp.read()
-    print(data_received)
+conn.request('DELETE', '/a.txt')
+resp = conn.getresponse()
+printResponse(resp)
+sleep(1.0)
+
+conn.request('DELETE', '/a.txt')
+resp = conn.getresponse()
+printResponse(resp)
+sleep(2.0)
+
+conn.request('DELETE', '/')
+resp = conn.getresponse()
+printResponse(resp)
+
+header = {'Content-type': 'application/json'}
+info = {'variable': 'valor'}
+json_data = json.dumps(info)
+conn.request('PUT', '/', json_data, header)
+resp = conn.getresponse()
+printResponse(resp)
+sleep(1.0)
+
+conn.request('CONNECT', '/')
+resp = conn.getresponse()
+printResponse(resp)
+
+conn.request('TRACE', '/')
+resp = conn.getresponse()
+printResponse(resp)
+
+conn.request('OPTIONS', '/')
+resp = conn.getresponse()
+printResponse(resp)
 
 conn.close()
--- http/http_server.py
+++ http/http_server.py
@@ -1,4 +1,7 @@
+import json
+import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from io import BytesIO
 
 hostName = "localhost"
 serverPort = 8000
@@ -6,28 +9,108 @@ serverPort = 8000
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            # send code 200 response
-            self.send_response(200)
+            rootdir = os.path.dirname(os.path.realpath(__file__))
+            if self.path is '/':                              # index.html
+                f = open(rootdir + '/index.html')
+            else:
+                f = open(rootdir + self.path)                 # Abre el archivo
 
-            # send header first
-            self.send_header('Content-type', 'text-html')
+            self.send_response(200)                           # OK
+
+            self.send_header('Content-type', 'text-html')     # Envía cabecera
             self.end_headers()
 
-            # send file content to client
-            self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-            self.wfile.write(bytes("<body>", "utf-8"))
-            self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-            self.wfile.write(bytes("</body></html>", "utf-8"))
+            self.wfile.write(f.read().encode())               # Envía el archivo
+            f.close()
+            return
+        except IOError:
+            self.send_error(404, 'No encontrado')             # No se encontró el archivo
+            return
+
+    def do_HEAD(self): # Retorna solo la cabecera del recurso
+        try:
+            rootdir = os.path.dirname(os.path.realpath(__file__))
+
+            if self.path is '/':                              # index.html
+                f = open(rootdir + '/index.html')
+            else:
+                f = open(rootdir + self.path)                 # Abre el archivo
+            f.close
+            self.send_response(200)                           # OK
+            self.end_headers()
             return
 
         except IOError:
-            self.send_error(404, 'file not found')
+            self.send_error(404, 'No encontrado')                 # No se encontró el recurso
 
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        content_len = int(self.headers['Content-Length'])
+        post_body = self.rfile.read(content_len)
+        self.wfile.write('received post request: {}'.format(post_body).encode())
+
+    def do_PUT(self):
+        content_len = int(self.headers['Content-Length'])
+        post_body = self.rfile.read(content_len)
+        j = json.dumps(post_body.decode())
+        print(j)
+
+        with open('data.json', 'w') as outfile:
+            json.dump(j, outfile)
+
+        self.send_response(201)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        self.wfile.write('received post request: {}'.format(post_body).encode())
+
+    def do_DELETE(self):
+        try:
+            rootdir = os.path.dirname(os.path.realpath(__file__))
+            if self.path is '/' or self.path.endswith('index.html'):
+                self.send_error(405)
+                return
+            f = open(rootdir + self.path)
+            f.close()
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            os.remove(rootdir + self.path)
+            self.wfile.write('Eliminado {} del servidor'.format(self.path).encode())
+            return
+
+        except IOError:
+            self.send_error(404, 'no encontrado')
+
+    def do_CONNECT(self):
+        self.send_error(501)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        self.wfile.write(b'No hay opciones que mostrar')
+
+    def do_OPTIONS(self):
+        self.send_error(501)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        self.wfile.write(b'No hay opciones que mostrar')
+
+    def do_TRACE(self):
+        self.send_error(501)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        self.wfile.write(b'No hay opciones que mostrar')
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    print("Servidor iniciado http://%s:%s" % (hostName, serverPort))
 
     try:
         webServer.serve_forever()
